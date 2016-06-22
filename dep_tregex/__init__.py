d6940969769f0ad914b02b11b63674d9c0b4526a
esgv <esgv@yandex-team.ru>
2016-06-22 10:56:31 +0300

    Docs in gh-pages.

--- dep_tregex/__init__.py
+++
@@ -1,7 +0,0 @@
-from dep_tregex.conll import *
-from dep_tregex.tree import *
-from dep_tregex.tree_action import *
-from dep_tregex.tree_pattern import *
-from dep_tregex.tree_script import *
-from dep_tregex.tree_state import *
-from dep_tregex.tree_to_html import *
